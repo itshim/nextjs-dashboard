0278ccfe21beb89c22dd331ac1b6385094d636f2
Himanshu Mishra <himanshu.mishra@infoedge.com>
2024-04-30 10:19:16 +0530

    added server functions, validations and liniting

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -159,13 +159,13 @@ export async function fetchInvoiceById(id: string) {
       FROM invoices
       WHERE invoices.id = ${id};
     `;
+    console.log("data",data);
 
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
